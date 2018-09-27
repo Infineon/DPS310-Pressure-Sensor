c511e57ae3fb2c1b16fb625ada1a977220ccc008
Manuel Hollfelder <manuel.hollfelder@infineon.com>
2018-09-27 15:18:15 +0200

    Fix compilation error
    FIX: included #include<Arduino.h>

--- src/Dps310.cpp
+++ src/Dps310.cpp
@@ -13,7 +13,7 @@
 
 
 #include "Dps310.h"
-
+#include <Arduino.h>
 
 const int32_t Dps310::scaling_facts[DPS310__NUM_OF_SCAL_FACTS]
 	= {524288, 1572864, 3670016, 7864320, 253952, 516096, 1040384, 2088960};
