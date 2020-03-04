888200c7efd8edb19ce69a2144e28ba31cdad449
Manuel Hollfelder <manuel.hollfelder@infineon.com>
2020-03-04 09:30:34 +0100

    Moved restart check before reading out the sensor

--- examples/i2c_command_with_restart/i2c_command_with_restart.ino
+++ examples/i2c_command_with_restart/i2c_command_with_restart.ino
@@ -3,7 +3,7 @@
 /* Example of the DPS310 Pressure Sensor which restarts the software interface 
  * if an error occurs. If an error code is given back on a DPS310 operation, 
  * the sensor handler is resetted and sensor initialisation is started again.
- */ 
+ */
 
 // The I2C address of the DPS310 pressure sensor (0x77 or 0x76)
 // Change here to set different address
@@ -23,143 +23,157 @@ uint16_t numberRestartTries = 0;
 // This function is executed once
 void setup()
 {
-  // Initialize the Arduino serial interface and wait until the interface is ready.
-  Serial.begin(9600);
-  while (!Serial);
+    // Initialize the Arduino serial interface and wait until the interface is ready.
+    Serial.begin(9600);
+    while (!Serial)
+        ;
 
-  // Call Dps310PressureSensor.begin to initialize DPS310 Pressure Sensor
-  // Wire is the I2C interface which is handed over to the DPS310 sensor class
+    // Call Dps310PressureSensor.begin to initialize DPS310 Pressure Sensor
+    // Wire is the I2C interface which is handed over to the DPS310 sensor class
 
-  // Use the commented line below to use the default I2C address (do not use this line and the above one simultaneously).
-  Dps310PressureSensor.begin(Wire, DPS_ADDRESS);
+    // Use the commented line below to use the default I2C address (do not use this line and the above one simultaneously).
+    Dps310PressureSensor.begin(Wire, DPS_ADDRESS);
 
-  // Initialization of pressure sensor done - reply on serial interface
-  Serial.println("Init complete!");
+    // Initialization of pressure sensor done - reply on serial interface
+    Serial.println("Init complete!");
 }
 
 // This function is continuously executed
 void loop()
 {
 
-  /*
-   * Read out the pressure sensor and print values on the serial interface
-   */
+   /*
+    * Read out the pressure sensor and print values on the serial interface
+    */
 
-  // Variables for storing the results
-  float temperature;
-  float pressure;
+    // Variables for storing the results
+    float temperature;
+    float pressure;
 
-  // The oversampling setting
-  uint8_t oversampling = 7;
+    // The oversampling setting
+    uint8_t oversampling = 7;
 
-  // The return error code
-  int16_t ret;
+    // The return error code
+    int16_t ret;
 
-  // Print an empty line on the serial interface
-  Serial.println();
+    // Print an empty line on the serial interface
+    Serial.println();
 
-  // The following command makes the DPS310 perform a single temperature measurement 
-  // with the last (or standard) configuration and the result is written to temperature.
+    // Only read-out the sensor if no restart is required
+    if ((restartRequired == false))
+    {
 
-  // ret = Dps310PressureSensor.measureTempOnce(temperature);
+        // The following command makes the DPS310 perform a single temperature measurement
+        // with the last (or standard) configuration and the result is written to temperature.
 
-  // The commented line below does exactly the same as the one above, but you can also 
-  // config the oversampling which can be a value from 0 to 7
-  // The DPS310 will perform 2^oversampling internal temperature measurements and combine 
-  // them to one result with higher precision (but measurement takes longer).
-  // Consult the datasheet for more information
+        // ret = Dps310PressureSensor.measureTempOnce(temperature);
 
-  ret = Dps310PressureSensor.measureTempOnce(temperature, oversampling);
+        // The commented line below does exactly the same as the one above, but you can also
+        // config the oversampling which can be a value from 0 to 7
+        // The DPS310 will perform 2^oversampling internal temperature measurements and combine
+        // them to one result with higher precision (but measurement takes longer).
+        // Consult the datasheet for more information
 
-  // Check the return code from the measureTempOnce() execution
-  // Enter only the following conditional if no restart is required (restartRequired == false)
-  if ( (ret != DPS__SUCCEEDED) && (restartRequired == false) )
-  {
-    // Something went wrong
-    // Look at the library code /src/util/dps_config.h for more information about return status codes
-    Serial.print("FAIL TEMPERATURE! ret = ");
-    Serial.println(ret);
+        ret = Dps310PressureSensor.measureTempOnce(temperature, oversampling);
 
-    // Something went wrong, we have to restart the sensor interface
-    restartRequired = true;
+        // Check the return code from the measureTempOnce() execution
+        // Enter only the following conditional if no restart is required (restartRequired == false)
+        if (ret != DPS__SUCCEEDED)
+        {
+            // Something went wrong
+            // Look at the library code /src/util/dps_config.h for more information about return status codes
+            Serial.print("FAIL TEMPERATURE! ret = ");
+            Serial.println(ret);
 
-  } else if ( (ret == DPS__SUCCEEDED) && (restartRequired == false) )
-  {
-    // Everything is all right, no error has been returned and no restart is pending (restartRequired == false)
-    Serial.print("Temperature: ");
-    Serial.print(temperature);
-    Serial.println(" degrees of Celsius");
-  }
+            // Something went wrong, we have to restart the sensor interface
+            restartRequired = true;
+        }
+        else if (ret == DPS__SUCCEEDED)
+        {
+            // Everything is all right, no error has been returned and no restart is pending (restartRequired == false)
+            Serial.print("Temperature: ");
+            Serial.print(temperature);
+            Serial.println(" degrees of Celsius");
+        }
+    }
 
-  // Pressure measurement function is similar like temperature measurement
-  // Use the following line if oversampling shall not be configured
-  // ret = Dps310PressureSensor.measurePressureOnce(pressure);
+    // Only read-out the sensor if no restart is required
+    // Do not read out the sensor or interact with it if a restart is required as an error occurred earlier
+    if ((restartRequired == false))
+    {
 
-  ret = Dps310PressureSensor.measurePressureOnce(pressure, oversampling);
+        // Pressure measurement function is similar like temperature measurement
+        // Use the following line if oversampling shall not be configured
+        // ret = Dps310PressureSensor.measurePressureOnce(pressure);
 
-  // Check the return code from the measurePressureOnce() execution
-  // Enter only the following conditional if no restart is required (restartRequired == false)
-  if ( (ret != DPS__SUCCEEDED) && (restartRequired == false))
-  {
-    // Something went wrong
-    // Look at the library code /src/util/dps_config.h for more information about return status codes
-    Serial.print("FAIL PRESSURE! ret = ");
-    Serial.println(ret);
+        ret = Dps310PressureSensor.measurePressureOnce(pressure, oversampling);
 
-    // Something went wrong, we have to restart the sensor software interface
-    restartRequired = true;
+        // Check the return code from the measurePressureOnce() execution
+        // Enter only the following conditional if no restart is required (restartRequired == false)
+        if (ret != DPS__SUCCEEDED)
+        {
+            // Something went wrong
+            // Look at the library code /src/util/dps_config.h for more information about return status codes
+            Serial.print("FAIL PRESSURE! ret = ");
+            Serial.println(ret);
 
-  } else if ( (ret == DPS__SUCCEEDED) && (restartRequired == false) )
-  {
-    // Everything is all right, no error has been returned and no software sensor interface restart is pending (restartRequired == false)
-    Serial.print("Pressure: ");
-    Serial.print(pressure);
-    Serial.println(" Pascal");
-  }
+            // Something went wrong, we have to restart the sensor software interface
+            restartRequired = true;
+        }
+        else if (ret == DPS__SUCCEEDED)
+        {
+            // Everything is all right, no error has been returned and no software sensor interface restart is pending (restartRequired == false)
+            Serial.print("Pressure: ");
+            Serial.print(pressure);
+            Serial.println(" Pascal");
+        }
+    }
 
   /*
    * Check the restart conditional and execute it if needed
    */
 
-  // Check whether restart is required and execute it
-  if ( (restartRequired == true) ){
-    // Increment the number of restart tries
-    numberRestartTries++;
-
-    // Set the DPS310 to idle modus and stop the sensor interface
-    ret = Dps310PressureSensor.standby();
-
-    // Check whether the sensor software interface could be stopped
-    if (ret != DPS__SUCCEEDED)
+    // Check whether restart is required and execute it
+    // Do not read out the sensor or interact with it if a restart is required as an error occurred earlier
+    if ((restartRequired == true))
     {
-      // Something went wrong when putting the device into idle state
-      // Look at the library code /src/util/dps_config.h for more information about return status codes
-      Serial.print("FAIL Restart! ret = ");
-      Serial.println(ret);
-      Serial.print("Restarting the sensor software interface FAILED, current restart tries: ");
-      Serial.println(numberRestartTries);
-      delay(50);
-      
-      Serial.println("Will try to execute begin() although restart failed.");
-      Serial.println("CAUTION: No proper restart cycling.");
-      Dps310PressureSensor.begin(Wire, DPS_ADDRESS);
+        // Increment the number of restart tries
+        numberRestartTries++;
 
-      // Something went wrong, we have to continue restarting the sensor software interface
-      restartRequired = false;
+        // Set the DPS310 to idle modus and stop the sensor interface
+        ret = Dps310PressureSensor.standby();
 
-    } else if ( ret == DPS__SUCCEEDED )
-    {
-      Dps310PressureSensor.begin(Wire, DPS_ADDRESS);
-      Serial.print("Restarting the sensor software interface SUCCEEDED, current restart tries: ");
-      Serial.println(numberRestartTries);
-      delay(50);
+        // Check whether the sensor software interface could be stopped
+        if (ret != DPS__SUCCEEDED)
+        {
+            // Something went wrong when putting the device into idle state
+            // Look at the library code /src/util/dps_config.h for more information about return status codes
+            Serial.print("FAIL Restart! ret = ");
+            Serial.println(ret);
+            Serial.print("Restarting the sensor software interface FAILED, current restart tries: ");
+            Serial.println(numberRestartTries);
+            delay(50);
 
-      // Everything is all right, we can continue with reading out the sensor
-      restartRequired = false;
+            Serial.println("Will try to execute begin() although restart failed.");
+            Serial.println("CAUTION: No proper restart cycling.");
+            Dps310PressureSensor.begin(Wire, DPS_ADDRESS);
+
+            // Something went wrong, we have to continue restarting the sensor software interface
+            restartRequired = false;
+        }
+        else if (ret == DPS__SUCCEEDED)
+        {
+            Dps310PressureSensor.begin(Wire, DPS_ADDRESS);
+            Serial.print("Restarting the sensor software interface SUCCEEDED, current restart tries: ");
+            Serial.println(numberRestartTries);
+            delay(50);
+
+            // Everything is all right, we can continue with reading out the sensor
+            restartRequired = false;
+        }
+        // Continue with the loop() function
     }
-    // Continue with the loop() function
-  }
-  
-  // Wait some time before starting a new measurement
-  delay(500);
+
+    // Wait some time before starting a new measurement
+    delay(500);
 }
